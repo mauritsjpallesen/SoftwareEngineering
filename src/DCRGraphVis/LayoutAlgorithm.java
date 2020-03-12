be5e9117b4c9ca4e385cff42e4dca7c6c9cccd93
Maurits <mauritsjpallesen@gmail.com>
2020-03-13 00:04:09 +0100

    housekeeping

--- src/DCRGraphVis/LayoutAlgorithm.java
+++ src/DCRGraphVis/LayoutAlgorithm.java
@@ -1,27 +1,14 @@
 package DCRGraphVis;
 
-import javafx.beans.binding.MapBinding;
 import oodcr.DCRGraph;
 import oodcr.Event;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-
 
 import org.jgrapht.*;
-import org.jgrapht.alg.drawing.model.Box2D;
-import org.jgrapht.alg.drawing.model.LayoutModel2D;
 import org.jgrapht.alg.drawing.model.Point2D;
 import org.jgrapht.graph.*;
 import  org.jgrapht.alg.drawing.*;
-//import org.jgrapht.nio.*;
-//import org.jgrapht.nio.dot.DOTExporter;
-import org.jgrapht.traverse.*;
-
-import java.io.*;
-import java.net.*;
 import java.util.*;
 
 public class LayoutAlgorithm {
@@ -79,30 +66,14 @@ public class LayoutAlgorithm {
             }
         }
 
-        var layoutAlg = new IndexedFRLayoutAlgorithm2D();
+        var layoutAlg = new FRLayoutAlgorithm2D<Event, DefaultEdge>();
         var layout = new LayoutModel(events.size());
         layoutAlg.layout(g, layout);
-
         var retVal = new ArrayList<Node>();
 
         for (Map.Entry<Event, Point2D> tmp : layout)
             retVal.add(new Node((int) tmp.getValue().getX(), (int) tmp.getValue().getY(), tmp.getKey()));
 
         return retVal;
-    }
-
-    public ArrayList<Node> generateNodes1(DCRGraph graph) {
-        var events = graph.events.values().toArray(new Event[0]);
-        var grid = new Node[events.length][events.length];
-        var center = (int)(events.length / 2);
-
-        Arrays.sort(events, Comparator.comparing(Event::getRelationsCount));
-        Collections.reverse(Arrays.asList(events));
-
-        for (var i = 0; i < events.length; i++) {
-            //placeEventNearestCenter(grid, center, )
-        }
-
-        return new ArrayList<Node>();
     }
 }
