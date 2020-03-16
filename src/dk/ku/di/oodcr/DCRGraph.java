384b9c0f0e4cc6f0a7f1ae727b84eb0bc8ed4a6f
Denis Drobny <drobny@zoho.com>
2020-03-16 18:38:18 +0100

    Slight modifications in Event's relationships representation.

--- src/dk/ku/di/oodcr/DCRGraph.java
+++ src/dk/ku/di/oodcr/DCRGraph.java
@@ -3,101 +3,107 @@ package dk.ku.di.oodcr;
 import java.util.HashMap;
 
 public class DCRGraph {
-	public HashMap<String, Event> events = new HashMap<>();
-	
-	public DCRGraph() { 
-	}
-	
-	public Event addEvent(String n)
-	{
-		return addEvent(n, n);
-	}	
-	
-	public Event addEvent(String n, String l)
-	{
-		Event e = new Event(n, l);
-		events.put(n, e);
-		return e;
-	}
-	
-	public void addEvent(Event e)
-	{
-		events.put(e.name, e);
-	}
+    public HashMap<String, Event> events = new HashMap<String, Event>();
 
-	// src -->* trg
-	public void addCondition(String src, String trg)
-	{
-		if (! events.containsKey(src))
-			return;
-		
-		if (! events.containsKey(trg))
-			return;
-		
-		events.get(trg).conditions.add(events.get(src));		
-	}
-	
-	// src --><> trg
-	public void addMilestone(String src, String trg)
-	{
-		if (! events.containsKey(src))
-			return;
-		
-		if (! events.containsKey(trg))
-			return;
-		
-		events.get(trg).milestones.add(events.get(src));		
-	}
-		
-	// src *--> trg
-	public void addResponse(String src, String trg)
-	{
-		if (! events.containsKey(src))
-			return;
-		
-		if (! events.containsKey(trg))
-			return;
-		
-		events.get(src).responses.add(events.get(trg));
-	}
-	
-	// src -->+ trg
-	public void addInclude(String src, String trg)
-	{
-		if (! events.containsKey(src))
-			return;
-		
-		if (! events.containsKey(trg))
-			return;
-		
-		events.get(src).includes.add(events.get(trg));		
-	}
+    public DCRGraph() {
+    }
 
-	// src -->% trg
-	public void addExclude(String src, String trg)
-	{
-		if (! events.containsKey(src))
-			return;
-		
-		if (! events.containsKey(trg))
-			return;
-		
-		events.get(src).excludes.add(events.get(trg));		
-	}
-	
-	public void execute(String e)
-	{
-		if (! events.containsKey(e))
-			return;		
-		events.get(e).execute();
-	}
-	
-	public Boolean isAccepting()
-	{
-		for (Event e : events.values())
-			if (!e.isAccepting())
-				return false;
+    public Event addEvent(String n) {
+        return addEvent(n, n);
+    }
 
-		return true;	
-	}
+    public Event addEvent(String n, String l) {
+        Event e = new Event(n, l);
+        events.put(n, e);
+        return e;
+    }
+
+    public void addEvent(Event e) {
+        events.put(e.name, e);
+    }
+
+    public void addRelationship(String src, String trg, RelationshipType relationshipType) {
+        if (!events.containsKey(src))
+            return;
+
+        if (!events.containsKey(trg))
+            return;
+
+        events.get(src).addRelationship(relationshipType, events.get(trg));
+    }
+
+    // These methods are here for backward compatibility, TODO refactor
+
+    // src -->* trg
+    public void addCondition(String src, String trg) {
+        if (!events.containsKey(src))
+            return;
+
+        if (!events.containsKey(trg))
+            return;
+
+        events.get(src).addRelationship(RelationshipType.CONDITIONS, events.get(trg));
+    }
+
+    // src --><> trg
+    public void addMilestone(String src, String trg) {
+        if (!events.containsKey(src))
+            return;
+
+        if (!events.containsKey(trg))
+            return;
+
+        events.get(src).addRelationship(RelationshipType.MILESTONES, events.get(trg));
+    }
+
+
+    // src *--> trg
+    public void addResponse(String src, String trg) {
+        if (!events.containsKey(src))
+            return;
+
+        if (!events.containsKey(trg))
+            return;
+
+        events.get(src).addRelationship(RelationshipType.RESPONSES, events.get(trg));
+    }
+
+
+    // src -->+ trg
+    public void addInclude(String src, String trg) {
+        if (!events.containsKey(src))
+            return;
+
+        if (!events.containsKey(trg))
+            return;
+
+        events.get(src).addRelationship(RelationshipType.INCLUDES, events.get(trg));
+    }
+
+
+    // src -->% trg
+    public void addExclude(String src, String trg) {
+        if (!events.containsKey(src))
+            return;
+
+        if (!events.containsKey(trg))
+            return;
+
+        events.get(src).addRelationship(RelationshipType.EXCLUDES, events.get(trg));
+    }
+
+
+    public void execute(String e) {
+        if (!events.containsKey(e))
+            return;
+        events.get(e).execute();
+    }
+
+    public Boolean isAccepting() {
+        for (Event e : events.values())
+            if (!e.isAccepting())
+                return false;
+
+        return true;
+    }
 }
